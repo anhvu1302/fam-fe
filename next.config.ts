3d37f3f507c8ad6ceedaf55b3639dffa347c1874
Vũ Văn Anh <vuvananh010203@gmail.com>
2026-01-06 14:34:07 +0700

    chore: init project

---
+++ next.config.ts
@@ -0,0 +1,29 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  basePath: process.env.BASEPATH,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/en/dashboards/crm',
+        permanent: true,
+        locale: false
+      },
+      {
+        source: '/:lang(en)',
+        destination: '/:lang/dashboards/crm',
+        permanent: true,
+        locale: false
+      },
+      {
+        source: '/:path((?!en|images|api|favicon.ico).*)*',
+        destination: '/en/:path*',
+        permanent: true,
+        locale: false
+      }
+    ]
+  }
+}
+
+export default nextConfig
